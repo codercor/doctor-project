c185fadb9c8d7f89457f1b2a105d5ec97a5b7313
Mustafa Çor <corxjs@gmail.com>
2022-10-11 00:56:37 +0300

    button fix

--- components/Card/HomeFirstSectionCard.tsx
+++ components/Card/HomeFirstSectionCard.tsx
@@ -7,7 +7,7 @@ const HomeFirstSectionCard = () => {
             <Text type="bold" className="text-[34px] text-onSecondary">Fonksiyonel Tıp Yoluyla Sağlığınızı Optimize Edin</Text>
             <Text type="light" className="text-white ">Fonksiyonel Tıp, hastalığın temel nedenlerini ele almak için bütünsel, hasta merkezli bir yaklaşım sunar. Mutlu ve canlı bir yaşam yaratmak için sağlığınızın sorumluluğunu üstlenmenize yardımcı oluyorum.</Text>
             <Button className="bg-onSecondary w-[168px] h-[61px] px-0">
-                <Text className="text-secondary" type="bold">Eğitimlere Göz at</Text>
+                <Text className="text-secondary px-0" type="regular">Eğitimlere Göz at</Text>
             </Button>
             <div className="absolute bottom-0 right-0">
                 <Image src="/images/svg/logo-v2-for-card.svg" height={134} width={242} />
