084e8d1c51a393919881b25370b172688b5863dc
Mustafa Çor <corxjs@gmail.com>
2022-10-25 14:10:35 +0300

    banner responsive fix

--- src/components/Section/FirstBanner.tsx
+++ src/components/Section/FirstBanner.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const FirstBanner = () => {
     return (
-        <Container className=" md:h-[1040px] h-[324px] brightness-[80%]  w-full bg-cover bg-no-repeat md:max-w-full bg-[url(/images/png/nazan-mobile.png)]  md:bg-cover md:bg-[url(/images/png/nazan.png)]">
+        <Container className=" md:h-[1040px] h-[324px] brightness-[80%]  w-full bg-cover bg-no-repeat md:!max-w-full bg-[url(/images/png/nazan-mobile.png)]  md:bg-cover md:bg-[url(/images/png/nazan.png)]">
             <Container className="md:!max-w-[1455px] md:flex flex-col hidden items-end pt-[314px] ">
                 <div className="bg-primary-flat flex flex-col items-start gap-[10px]  bg-opacity-60 w-[630px] h-auto pl-[34px] rounded-t-[5px] rounded-b-[20px] py-[42px]">
                     <Text className="text-[white] " type="h4">Fonksiyonel Beslenme Yoluyla Sağlığınızı Optimize Edin</Text>
