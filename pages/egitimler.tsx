2599241ece161e4393a5819112926d191e4295bb
Mustafa Çor <corxjs@gmail.com>
2022-10-25 23:28:03 +0300

    container bug fix

--- pages/egitimler.tsx
+++ pages/egitimler.tsx
@@ -153,7 +153,7 @@ const Egitimler = () => {
                     <Navbar />
                 </Container>
             </Container>
-            <Container className=" h-[300px] md:h-[300px]  w-full bg-cover bg-no-repeat md:max-w-full bg-right-bottom  md:bg-cover bg-[url(/images/png/hakkimda-bg.png)]">
+            <Container className=" h-[300px] md:h-[300px]  !w-full bg-cover bg-no-repeat md:max-w-full bg-right-bottom  md:bg-cover bg-[url(/images/png/hakkimda-bg.png)]">
                 <Container className="md:!max-w-[1455px] grid  place-items-end  justify-center pb-20 md:pb-22 h-full">
                     <Text className="text-[#F2F2F2] text-[24px] md:text-[34px] font-nexa-bold">Eğitimler</Text>
                 </Container>
