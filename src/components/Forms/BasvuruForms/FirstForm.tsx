249d70f396c7b70dddc5c3a6e2963e0b9a571476
Mustafa Çor <corxjs@gmail.com>
2022-12-25 16:55:10 +0300

    forms start

--- src/components/Forms/BasvuruForms/FirstForm.tsx
+++ src/components/Forms/BasvuruForms/FirstForm.tsx
@@ -6,7 +6,7 @@ import * as Yup from 'yup';
 const validationSchema = Yup.object({ //min >= 2 words required
     fullName: Yup.string().required("Zorunlu alan").matches(/^[a-zA-Z]+ [a-zA-Z]+$/, "Adınız ve Soyadınızı giriniz"),
     age: Yup.number().required("Zorunlu alan").min(10, "Yaşınız 10'dan küçük olamaz"),
-    gender: Yup.string().required("Zorunlu alan").oneOf(['kadın', 'erkek', 'gey'], "Cinsiyet seçiniz")
+    gender: Yup.string().required("Zorunlu alan").oneOf(['kadın', 'erkek', 'diger'], "Cinsiyet seçiniz")
 });
 
 export default function FirstForm() {
@@ -26,7 +26,7 @@ export default function FirstForm() {
                         <FormInputSelect error={errors.gender} name="gender" value={values.gender} onChange={handleChange} label='Cinsiyetiniz' options={[
                             { value: 'kadın', label: 'Kadın' },
                             { value: 'erkek', label: 'Erkek' },
-                            { value: 'gey', label: 'Gey' },
+                            { value: 'diger', label: 'Diğer' },
                             ]} />
                     </div>
                     <FormDivider />
--- types.ts
+++ types.ts
@@ -1,8 +1,9 @@
-type AppointmentType = {
-    name: "Ön Değerlendirme Formu"
-    | "IFM Beslenme Öyküsü Formu"
-    | "IFM Erkek Değerlendirme Formu"
-    | "IFM Kadın Değerlendirme Formu"
-    | "Tıbbi Şikayet Değerlendirme Formu",
-    id: number
-}
+// type AppointmentType = {
+//     Name: "Ön Değerlendirme Formu"
+//     | "IFM Beslenme Öyküsü Formu"
+//     | "IFM Erkek Değerlendirme Formu"
+//     | "IFM Kadın Değerlendirme Formu"
+//     | "Tıbbi Şikayet Değerlendirme Formu",
+//     Status: "Pending" | "Confirmed" | "Rejected" | "InReview",
+    
+// }
