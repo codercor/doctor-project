2f234f80766be0c52c953065bb483cc310562967
Mustafa Çor <mmustafaa9@gmail.com>
2023-02-12 12:32:50 +0300

    clarity update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,10 +16,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <Script id="clarity" type="text/javascript">
-          {` (function(c,l,a,r,i,t,y){
-            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "fnmj0v9z8s");`}
         </Script>
         <Component {...pageProps} />
