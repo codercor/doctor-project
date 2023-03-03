4d6d29b98009c67fa1d571aa78a77cddae12f08e
Mustafa Çor <mmustafaa9@gmail.com>
2023-03-03 21:26:03 +0300

    Update chat.tsx

--- pages/dashboard/chat.tsx
+++ pages/dashboard/chat.tsx
@@ -4,7 +4,7 @@ import SettingsMenuButton from "@components/Button/SettingsMenuButton";
 import Input from "@components/Input/Input";
 import DashboardLayout from "@components/Layouts/DashboardLayout";
 import Text from "@components/Text";
-import { Star, SendSharp, Refresh } from "@mui/icons-material";
+import { Star, SendSharp, Refresh, ArrowBackIos } from "@mui/icons-material";
 import classNames from "classnames";
 import { useEffect, useState } from "react";
 import { useChat } from "src/hooks/chat.hook";
@@ -19,7 +19,7 @@ const ChatUserCard = ({ chatLine, onClick, active }: { chatLine: ChatLine, onCli
 
     return <div onClick={() => {
         onClick()
-    }} className={classNames("bg-[#D0E4E8] transition-all hover:bg-[white] duration-1000 hover:rounded-[5px_20px] hover:border-2  h-[97px] flex items-center justify-start p-[20px]", {
+    }} className={classNames("bg-[#D0E4E8] w-full transition-all hover:bg-[white] duration-1000 hover:rounded-[5px_20px] hover:border-2  h-[97px] flex items-center justify-start p-[20px]", {
         "!bg-[white] transtion-all  border-2 rounded-[20px_5px] border-[#D0E4E8]": active
     })}>
         <div className="bg-[#4E929D] w-[60px] h-[60px] rounded-full text-[white] grid place-content-center">
@@ -36,7 +36,7 @@ const ChatMessage = ({ isMe = true, message }: ChatMessageProps) => {
     return <div className={classNames("flex p-[20px] h-fit mt-[20px] w-[80%] ", {
         "self-end": !isMe,
     })}>
-        {isMe && <div className="flex text-[white] items-center justify-center min-w-[60px] h-[60px] rounded-full bg-secondary">
+        {isMe && <div className="flex text-[white] items-center justify-center md:min-w-[60px] min-w-[50px] h-[50px] md:h-[60px] rounded-full bg-secondary">
             <Star />
         </div>}
         <div className="h-fit mt-[36px] ml-2 leading-none  w-[calc(100%-80px)] bg-[white] pt-[3px] rounded-[10px_3px_10px_3px] pl-[5px]">
@@ -47,12 +47,12 @@ const ChatMessage = ({ isMe = true, message }: ChatMessageProps) => {
             })} </Text>
             <Text className="text-[16px] break-words max-h-auto h-fit">{message?.Message}</Text>
         </div>
-        {!isMe && <div className="flex  text-[white] items-center justify-center min-w-[60px] h-[60px] rounded-full bg-secondary">
+        {!isMe && <div className="flex  text-[white] items-center justify-center  md:min-w-[60px] min-w-[50px] h-[50px] md:h-[60px] rounded-full bg-secondary-flat">
             <Star />
         </div>}
     </div>
 }
-const ChatBox = () => {
+const ChatBox = ({ back }: { back: () => void }) => {
     const { chat: { Messages, SelectedChatLineId, ChatLines, }, sendMessage, getChatLineMessages } = useChat();
     const { user: { Id } } = useUser()
     const router = useRouter();
@@ -65,13 +65,17 @@ const ChatBox = () => {
 
     }, [Messages]);
     const [disabled, setDisabled] = useState(false);
-    return <div className="px-[30px] relative w-full h-full flex flex-col bg-[#F9FBFC]">
+    return <div className="px-[30px] md:relative absolute h-[70%]  w-full md:h-full flex flex-col bg-[#F9FBFC]">
         {SelectedChatLineId && <div onClick={() => {
             getChatLineMessages(SelectedChatLineId)
-        }} className="w-[40px] hover:shadow-md shadow-xl transition-all cursor-pointer grid place-content-center rounded-[5px_20px] absolute top-0 right-0 h-[40px] bg-secondary text-[white]">
+        }} className="w-[40px] hover:shadow-md shadow-xl transition-all cursor-pointer grid place-content-center rounded-[5px_20px] absolute top-0 right-1 h-[40px] bg-secondary text-[white]">
             <Refresh />
         </div>}
-
+        {SelectedChatLineId && <div onClick={() => {
+            back()
+        }} className="w-[40px] hover:shadow-md shadow-xl pl-2 transition-all cursor-pointer grid md:hidden place-content-center rounded-[5px_20px] absolute top-0 left-1 h-[40px] bg-secondary text-[white]">
+            <ArrowBackIos className="scale-90" />
+        </div>}
         <div className="w-full relative overflow-auto h-full flex flex-col-reverse">
 
             {Messages.map((item) => <ChatMessage key={v4()} message={item} isMe={!(item.SenderId == Id)} />)}
@@ -149,33 +153,36 @@ const Chat = () => {
         })
 
     }, [activeLineId]);
-    const isDesktop = useIsDesktop();
+
+    const back = () => {
+        setActiveLineId(null);
+    }
+
     return (
         <DashboardLayout>
-            {!isDesktop ? <div className="w-full h-full items-center justify-center flex p-[30px]">
-                <h1> Bu sayfayı görüntülemek için mobil cihazlar uygun değildir. </h1>
-            </div> :
-                <div className=" md:h-[798px] flex h-full  rounded-[30px_5px] bg-[#F4F4F4]">
-                    <div className="w-1/3 h-full flex relative flex-col text-start items-center justify-start py-[26px] px-[30px]">
-                        <div className="flex justify-between w-full">
-                            <Text type="h3" className="text-secondary !text-[20px] w-full">Mesajlar   </Text>
-                        </div>
 
-                        <div className="w-full h-full flex flex-col gap-[10px] overflow-auto  scrollbar-thumb-white-default scrollbar-thin scrollbar-track-indigo-100">
-
-                            {ChatLines.map((item) => <>
-                                {/* <> {item.ChatLineId}</> */}
-                                <ChatUserCard active={
-                                    activeLineId == item.ChatLineId
-                                } onClick={() => {
-                                    getChatLineMessages(item.ChatLineId);
-                                    setActiveLineId(item.ChatLineId);
-                                }} chatLine={item} key={v4()} /></>)}
-                        </div>
+            <div className=" md:h-[798px] flex h-full  rounded-[30px_5px] bg-[#F4F4F4]">
+                <div className="md:w-1/2 w-full h-full flex relative flex-col text-start items-center justify-start py-[26px] px-[30px]">
+                    <div className="flex justify-between w-full">
+                        <Text type="h3" className="text-secondary !text-[20px] w-full">Mesajlar   </Text>
                     </div>
 
-                    <ChatBox />
-                </div>}
+                    <div className="w-full h-full flex flex-col gap-[10px] overflow-auto  scrollbar-thumb-white-default scrollbar-thin scrollbar-track-indigo-100">
+                        {ChatLines.map((item) => <>
+                            {/* <> {item.ChatLineId}</> */}
+                            <ChatUserCard active={
+                                activeLineId == item.ChatLineId
+                            } onClick={() => {
+                                getChatLineMessages(item.ChatLineId);
+                                setActiveLineId(item.ChatLineId);
+                            }} chatLine={item} key={v4()} /></>)}
+                    </div>
+                </div>
+                <button className="absolute" onClick={() => {
+                    setActiveLineId(null);
+                }}> DEL </button>
+                {activeLineId && <ChatBox back={back} />}
+            </div>
         </DashboardLayout>
     );
 }
