16c34f0bcfd2b12c2e55259a52ae5374646b1bc2
Mustafa Çor <mmustafaa9@gmail.com>
2023-03-04 20:39:58 +0300

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,16 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                
+                <Script id="g-tag-1" async src="https://www.googletagmanager.com/gtag/js?id=G-D0HTKY3R5J"></Script>
+                <Script id="g-tag-2" strategy='lazyOnload'>
+                    {` <script>
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', 'G-D0HTKY3R5J');
+                    </script>`}
+                </Script>
             </Head>
             <body>
                 <Main />
