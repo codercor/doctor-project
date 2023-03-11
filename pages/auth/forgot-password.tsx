9cdbb2eefd031be8327a0156b120261625f281a1
Emir Burgazli <emirhanburgazli@gmail.com>
2023-03-11 15:11:15 +0300

    fix

--- pages/auth/forgot-password.tsx
+++ pages/auth/forgot-password.tsx
@@ -25,7 +25,8 @@ const Login = () => {
         let errors: ValidationErrors = {
             Email: null,
         }
-        if (credentials.Email === '' || /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(credentials.Email) === false) {
+        // if (credentials.Email === '' || /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(credentials.Email) === false) {
+            if (credentials.Email === '' || /^([_a-z0-9]+[\._a-z0-9]*)(\+[a-z0-9]+)?@(([a-z0-9-]+\.)*[a-z]{2,3})$/.test(credentials.Email) === false) {
             errors.Email = 'Geçerli bir eposta giriniz';
         } else {
             errors.Email = null;
