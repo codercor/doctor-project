65ae9e6e81185b1ba0c57344ea728f2d02622d44
Emir Burgazli <emirhanburgazli@gmail.com>
2023-03-15 10:48:48 +0300

    form value fix

--- src/components/Forms/SubSteps/SubStep2Part10.tsx
+++ src/components/Forms/SubSteps/SubStep2Part10.tsx
@@ -50,7 +50,7 @@ let currentDiet = [
 let livePoint = [
     { value: "n/a", label: "N/A" },
     { value: "1", label: "1" },
-    { value: "2", label: "3" },
+    { value: "2", label: "2" },
     { value: "3", label: "3" },
     { value: "4", label: "4" },
     { value: "5", label: "5" },
