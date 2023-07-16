04e17e444bd8b18680addc88f9e95a2b932b8845
Emir Burgazli <emirhanburgazli@gmail.com>
2023-07-16 05:01:55 +0300

    error message eklemesi yapıldı

--- pages/payment-result.tsx
+++ pages/payment-result.tsx
@@ -15,27 +15,73 @@ const Success = () => {
     </div>
 }
 
-const Failed = () => {
+const Failed = ({ errorCode }: { errorCode: string }) => {
+    const errorMessage = getErrorMessage(errorCode);
     return <div className="w-full h-full bg-primary-light">
         <div className="w-[300px] h-full text-center grid place-content-center mx-auto">
             <ErrorOutlineRounded className='min-w-[100px] animate-pulse mb-4 mx-auto min-h-[100px] text-[#b63b3b]' />
+            {errorCode === null} ?? (<p className='font-nexa-bold'>{errorMessage}</p><br />)
             <p className='font-nexa-bold'> Satın alım işleminiz gerçekleştirilememiştir. Kullanıcı paneline yönlendiriliyorsunuz... </p>
         </div>
     </div>
 }
 
+type ErrorMessagesType = {
+    [errorCode: string]: string;
+  };
+
+  
+const errorMessages:ErrorMessagesType = {
+    "10051": "Kart limiti yetersiz, yetersiz bakiye",
+    "10005": "İşlem onaylanmadı",
+    "10012": "Geçersiz işlem",
+    "10041": "Kayıp kart, karta el koyunuz",
+    "10043": "Çalıntı kart, karta el koyunuz",
+    "10054": "Vadesi dolmuş kart",
+    "10084": "CVC2 bilgisi hatalı",
+    "10057": "Kart sahibi bu işlemi yapamaz",
+    "10058": "Terminalin bu işlemi yapmaya yetkisi yok",
+    "10034": "Dolandırıcılık şüphesi",
+    "10093": "Kartınız e-ticaret işlemlerine kapalıdır. Bankanızı arayınız.",
+    "10201": "Kart, işleme izin vermedi",
+    "10204": "Ödeme işlemi esnasında genel bir hata oluştu",
+    "10206": "CVC uzunluğu geçersiz",
+    "10207": "Bankanızdan onay alınız",
+    "10208": "Üye işyeri kategori kodu hatalı",
+    "10209": "Bloke statülü kart",
+    "10210": "Hatalı CAVV bilgisi",
+    "10211": "Hatalı ECI bilgisi",
+    "10213": "BIN bulunamadı",
+    "10214": "İletişim veya sistem hatası",
+    "10215": "Geçersiz kart numarası",
+    "10216": "Bankası bulunamadı",
+    "10217": "Banka kartları sadece 3D Secure işleminde kullanılabilir",
+    "10219": "Bankaya gönderilen istek zaman aşımına uğradı",
+    "10222": "Terminal taksitli işleme kapalı",
+    "10223": "Gün sonu yapılmalı",
+    "10225": "Kısıtlı kart",
+    "10226": "İzin verilen PIN giriş sayısı aşılmış",
+    "10227": "Geçersiz PIN",
+    "10228": "Banka veya terminal işlem yapamıyor",
+    "10229": "Son kullanma tarihi geçersiz",
+  };
+
+  const getErrorMessage = (errorCode: string) => {
+    return errorMessages[errorCode] || "Bilinmeyen hata"; // Eğer errorCode listede yoksa "Bilinmeyen hata" döndürür
+  };
+
 export default function PaymentFailed() {
     const {
-        query: { Status },
+        query: { Status,ErrorCode },
         push
     } = useRouter()
     const [content, setContent] = React.useState(<></>)
     useEffect(() => {
         if (Status === "1") {
-            setContent(Success)
-        } else {
-            setContent(Failed)
-        }
+            setContent(<Success />);
+          } else {
+            setContent(<Failed errorCode={ErrorCode as string} />);
+          }
 
         setTimeout(() => {
             push('/dashboard')
@@ -44,7 +90,7 @@ export default function PaymentFailed() {
             });
         }, 4000)
 
-    }, [Status])
+    }, [Status,ErrorCode])
 
 
 
